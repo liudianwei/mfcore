b9651dfcc442846156fcc6d2f627148248c82bfd
chenlingbin <corbin_chen@macroinf.com>
2022-02-16 17:45:51 +0800

    v1.0.3-rc7 优化日志性能

--- MF/BC.BasicData/ApplicationLog.cs
+++ MF/BC.BasicData/ApplicationLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
 using System.IO;
@@ -20,7 +21,7 @@ namespace SystemFramework
         /// <summary>
         /// 日志跟踪路径
         /// </summary>
-        private static string TraceFolderPath { set; get; } = string.Empty;
+        public static string TraceFolderPath { set; get; } = string.Empty;
 
         /// <summary>
         /// 日志保留时间(天)
@@ -90,13 +91,28 @@ namespace SystemFramework
         }
 
         /// <summary>
+        /// 是否输出Console
+        /// </summary>
+        public static bool IsConsole = false;
+
+        /// <summary>
         ///
         /// </summary>
-        static ApplicationLog()
+        public ApplicationLog(bool isconsole = false)
         {
+            IsConsole = isconsole;
             //Protect thread locks with Try/Catch to guarantee that we let go of the lock.
             try
             {
+                string MES_Code = "";
+                try
+                {
+                    MES_Code = ConfigurationManager.AppSettings.GetValues("MES_Code")[0].ToString();
+                }
+                catch
+                {
+                    MES_Code = Guid.NewGuid().ToString();
+                }
                 try
                 {
                     TraceFolderPath = ConfigurationManager.AppSettings.GetValues("TraceFolderPath")[0].ToString();
@@ -106,7 +122,7 @@ namespace SystemFramework
                     TraceFolderPath = "C:\\Macroinf\\";
                 }
 
-                TraceFolderPath += "Macroinf_MisData_" + ConfigurationManager.AppSettings.GetValues("MES_Code")[0].ToString() + "\\";
+                TraceFolderPath += "Macro_" + MES_Code + "\\";
                 //See if there is a debug configuration file specified and set up the
                 //  tracing variables.
                 bool clearSettings = true;
@@ -155,9 +171,6 @@ namespace SystemFramework
             }
         }
 
-        //读写锁，当资源处于写入模式时，其他线程写入需要等待本次写入结束之后才能继续写入
-        private static ReaderWriterLockSlim LogWriteLock = new ReaderWriterLockSlim();
-
         /// <summary>
         /// 记录逻辑日志
         /// </summary>
@@ -167,31 +180,17 @@ namespace SystemFramework
         {
             try
             {
-                //设置读写锁为写入模式独占资源，其他写入请求需要等待本次写入结束之后才能继续写入
-                //注意：长时间持有读线程锁或写线程锁会使其他线程发生饥饿 (starve)。 为了得到最好的性能，需要考虑重新构造应用程序以将写访问的持续时间减少到最小。
-                //从性能方面考虑，请求进入写入模式应该紧跟文件操作之前，在此处进入写入模式仅是为了降低代码复杂度
-                //因进入与退出写入模式应在同一个try finally语句块内，所以在请求进入写入模式之前不能触发异常，否则释放次数大于请求次数将会触发异常
-                LogWriteLock.EnterWriteLock();
-
                 // 拼日志格式
                 string str = string.Format("{0}  {1}\r\n", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), strContentInfo);
-                string tmpLogPath = string.Format("{0}BusinessLog/{1}", TraceFolderPath, DateTime.Now.ToString("yyyyMMdd"));
-                if (!Directory.Exists(tmpLogPath)) Directory.CreateDirectory(tmpLogPath);
-                StreamWriter sw = File.AppendText(string.Format("{0}/{1}.log", tmpLogPath, fileName));
-                sw.Write(str); sw.Close();
-                Enqueue(new Msg() { Name = fileName, Content = str });
+                if (IsConsole)
+                {
+                    EnqueueConsole(new Msg() { Name = fileName, Content = str });
+                }
+                EnqueueLog(fileName, new Msg() { Name = fileName, Content = str });
             }
             catch (Exception e)
             {
                 throw e;
-            }
-            finally
-            {
-                //退出写入模式，释放资源占用
-                //注意：一次请求对应一次释放
-                //若释放次数大于请求次数将会触发异常[写入锁定未经保持即被释放]
-                //若请求处理完成后未释放将会触发异常[此模式不下允许以递归方式获取写入锁定]
-                LogWriteLock.ExitWriteLock();
             }
         }
 
@@ -246,29 +245,71 @@ namespace SystemFramework
             }
         }
 
-        #region 队列
+        #region Console队列
 
         /// <summary>
         /// 通知队列
         /// </summary>
-        public static ConcurrentQueue<Msg> concurrentQueue = new ConcurrentQueue<Msg>();
+        public static ConcurrentQueue<Msg> concurrentQueueConsole = new ConcurrentQueue<Msg>();
 
         /// <summary>
         /// 通知队列 信号量
         /// </summary>
-        public static AutoResetEvent autoResetQueue = new AutoResetEvent(false);
+        public static AutoResetEvent autoResetQueueConsole = new AutoResetEvent(false);
 
         /// <summary>
         ///写入消息队列
         /// </summary>
         /// <param name="msg"></param>
-        public static void Enqueue(Msg msg)
+        private static void EnqueueConsole(Msg msg)
         {
-            concurrentQueue.Enqueue(msg);
-            autoResetQueue.Set(); //通知task队列里有内容了  可以开始循环读取
+            concurrentQueueConsole.Enqueue(msg);
+            autoResetQueueConsole.Set(); //通知task队列里有内容了  可以开始循环读取
         }
 
-        #endregion 队列
+        #endregion Console队列
+
+        #region Log队列
+
+        /// <summary>
+        /// 通知队列
+        /// </summary>
+        public static Dictionary<string, QueueLog> QueueLogDic { set; get; } = new Dictionary<string, QueueLog>();
+
+        /// <summary>
+        /// 写入消息队列
+        /// </summary>
+        /// <param name="OpName"></param>
+        /// <param name="msg"></param>
+        public static void EnqueueLog(string OpName, Msg msg)
+        {
+            if (QueueLogDic.TryGetValue(OpName, out var queueLog))
+            {
+                queueLog.Enqueue(msg);
+            }
+            else
+            {
+                queueLog = new QueueLog(OpName);
+                queueLog.Enqueue(msg);
+                QueueLogDic.Add(OpName, queueLog);
+            }
+        }
+
+        /// <summary>
+        /// 初始化日志线程
+        /// </summary>
+        public static void IntLogThread(List<string> OpNames)
+        {
+            foreach (var item in OpNames)
+            {
+                if (!QueueLogDic.ContainsKey(item))
+                {
+                    QueueLogDic.Add(item, new QueueLog(item));
+                }
+            }
+        }
+
+        #endregion Log队列
     }
 
     /// <summary>
@@ -285,5 +326,101 @@ namespace SystemFramework
         ///
         /// </summary>
         public string Content { get; set; }
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class QueueLog
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public string Name = string.Empty;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public ConcurrentQueue<Msg> concurrentQueueLog = new ConcurrentQueue<Msg>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        public AutoResetEvent autoResetQueueLog = new AutoResetEvent(false);
+
+        private Thread thread = null;
+
+        //读写锁，当资源处于写入模式时，其他线程写入需要等待本次写入结束之后才能继续写入
+        private static ReaderWriterLockSlim LogWriteLock = new ReaderWriterLockSlim();
+
+        /// <summary>
+        ///
+        /// </summary>
+        public QueueLog(string name)
+        {
+            Name = name;
+            ThreadSaveFile();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="msg"></param>
+        public void Enqueue(Msg msg)
+        {
+            concurrentQueueLog.Enqueue(msg);
+            autoResetQueueLog.Set(); //通知task队列里有内容了  可以开始循环读取
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        private void ThreadSaveFile()
+        {
+            thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    autoResetQueueLog.WaitOne();
+                    while (concurrentQueueLog.Any())
+                    {
+                        if (concurrentQueueLog.TryDequeue(out var msg))
+                        {
+                            try
+                            {
+                                //设置读写锁为写入模式独占资源，其他写入请求需要等待本次写入结束之后才能继续写入
+                                //注意：长时间持有读线程锁或写线程锁会使其他线程发生饥饿 (starve)。 为了得到最好的性能，需要考虑重新构造应用程序以将写访问的持续时间减少到最小。
+                                //从性能方面考虑，请求进入写入模式应该紧跟文件操作之前，在此处进入写入模式仅是为了降低代码复杂度
+                                //因进入与退出写入模式应在同一个try finally语句块内，所以在请求进入写入模式之前不能触发异常，否则释放次数大于请求次数将会触发异常
+                                LogWriteLock.EnterWriteLock();
+
+                                string tmpLogPath = string.Format("{0}BusinessLog/{1}", ApplicationLog.TraceFolderPath, DateTime.Now.ToString("yyyyMMdd"));
+                                if (!Directory.Exists(tmpLogPath)) Directory.CreateDirectory(tmpLogPath);
+                                using (StreamWriter sw = File.AppendText(string.Format("{0}/{1}.log", tmpLogPath, msg.Name)))
+                                {
+                                    sw.Write(msg.Content);
+                                    sw.Close();
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                ApplicationLog.WriteLog(ex, $"{msg.Name} BusinessLog日志记录出错");
+                            }
+                            finally
+                            {
+                                //退出写入模式，释放资源占用
+                                //注意：一次请求对应一次释放
+                                //若释放次数大于请求次数将会触发异常[写入锁定未经保持即被释放]
+                                //若请求处理完成后未释放将会触发异常[此模式不下允许以递归方式获取写入锁定]
+                                LogWriteLock.ExitWriteLock();
+                            }
+                        }
+                    }
+                    autoResetQueueLog.Reset();
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
     }
 }
---
+++ MF/BC.BasicData/README.txt
@@ -0,0 +1,12 @@
+﻿v1.0.0 基础版
+v1.0.1 发布版
+v1.0.2-rc3 CustomeEvetnArgs补充字段
+v1.0.2-rc11 提高日志性能
+      本次升级相应修改业务如下:
+      new ApplicationLog();
+      List<string> OpNames= new List<string>();
+      OpNames.Add("OP010");
+      OpNames.Add("OP020");
+      ApplicationLog.IntLogThread(OpNames);
+v1.0.3-rc5 发布版
+v1.0.3-rc7 优化日志性能
