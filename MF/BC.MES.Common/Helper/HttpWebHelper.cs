558bab9d60b2ed447fa4b8da21048c607fac0448
corbin_chen@macroinf.com <corbin_chen@macroinf.com>
2022-10-14 14:55:20 +0800

    新增方法

--- MF/BC.MES.Common/Helper/HttpWebHelper.cs
+++ MF/BC.MES.Common/Helper/HttpWebHelper.cs
@@ -41,5 +41,49 @@ namespace Common.Helper
                 }
             }
         }
+		
+		/// <summary>
+        /// Request请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="data">传递的参数</param>
+        /// <param name="contentType">application/x-www-form-urlencoded</param>
+        /// <param name="Method">POST,GET等</param>
+        /// <param name="Timeout">请求超时</param>
+        /// <returns></returns>
+        public static string RequestHttp(string url, string data = "[]", 
+		    string contentType = "application/json;charset=utf-8", 
+			string Method = "POST",
+            int Timeout = 5000)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient() { Timeout = TimeSpan.FromMilliseconds(Timeout) })
+                {
+                    using (var request = new HttpRequestMessage(new HttpMethod(Method), url))
+                    {
+                        request.Headers.TryAddWithoutValidation("Content-Type", contentType);
+                        request.Content = new StringContent(data);
+                        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(contentType);
+                        HttpResponseMessage response = httpClient.SendAsync(request).Result;
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            using (HttpContent content = response.Content)
+                            {
+                                return content.ReadAsStringAsync().Result;
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception(response.RequestMessage.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
     }
 }
